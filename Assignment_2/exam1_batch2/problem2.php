bcbb2e78476af01359b89fdfd233a7980603cc6f
tajulislam <tajulislam8028@gmail.com>
2019-06-22 16:55:19 +0600

    some change

--- Assignment_2/exam1_batch2/problem2.php
+++ Assignment_2/exam1_batch2/problem2.php
@@ -5,11 +5,17 @@ $stdout = fopen('php://stdout', 'w');
 
 
 fscanf(STDIN,"%s",$str);
+//$str=fgets(STDIN);
 
 $sum=0;
+$str2="end";
+ 
+   // while()
 for($i=0;$i<strlen($str);$i++)
- {
+{  if(strcmp($str,$str2)==0)
+    break;
   $sum+=ord($str[$i]);
 
  }
  echo $sum;
+ 
